b43f86df8783d07b52beca852d9896570cd9d6fc
michael@outreachmagic.io <michael@outreachmagic.io>
2024-06-08 12:59:56 -0500

    Application almost ready, small details to review

--- src/auth/lib/mongodb/auth-mongodb.ts
+++ src/auth/lib/mongodb/auth-mongodb.ts
@@ -41,16 +41,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
           if (!user) {
             throw new Error('No user found');
           }
-
-          // const isValidPassword = await bcryptjs.compare(
-          //   credentials.password as string,
-          //   user.appLogin.password
-          // );
-
-          // if (!isValidPassword) {
-          //   throw new Error('Invalid password');
-          // }
-
           await db.collection('userSettings').updateOne(
             { 'appLogin.username': credentials.email },
             {
