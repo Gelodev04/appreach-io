b890e23f936569470903d3f583211f8e2f96fa4f
javi95z <javi.95.zgz@gmail.com>
2024-08-18 23:10:58 +0200

    Replace looker studio URL in dashboard

--- src/sections/host/utils/generate-looker-studio-url.ts
+++ src/sections/host/utils/generate-looker-studio-url.ts
@@ -1,7 +1,7 @@
-export function generateLookerStudioUrl(hostCryptIds: string[]): string {
+export function generateLookerStudioUrl(hostCrypts: string[]): string {
   const baseUrl = process.env.NEXT_PUBLIC_LOOKER_URL;
   if (!baseUrl) throw new Error('NEXT_PUBLIC_LOOKER_URL is not defined');
 
-  const idsString = hostCryptIds.join(',');
+  const idsString = hostCrypts.join(',');
   return baseUrl.replace('{}', idsString);
 }
