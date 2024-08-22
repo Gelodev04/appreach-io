a7341f03e77d493c3adeedf6dd0a95284804d4aa
javi95z <javi.95.zgz@gmail.com>
2024-08-22 19:01:25 +0200

    Remove commented out code

--- src/app/api/seed/counts/route.ts
+++ src/app/api/seed/counts/route.ts
@@ -12,13 +12,11 @@ export async function GET() {
   try {
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_DATABASE || undefined);
-    // const userSettings = await getUser();
 
     // Count all email accounts for each seed account type
     const seedAccounts = await Promise.all(
       types.map(async (name) => {
         const amount = await db.collection('emailAccounts').countDocuments({
-          // username: userSettings.appLogin.username,
           espCamelCase: name,
           'webMail.status': 'ready',
           'webMail.routine': 'engagementAccount',
