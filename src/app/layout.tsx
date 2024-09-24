ad535bf64fc374ea57d51c2299eb64f9a4e6b445
javi95z <javi.95.zgz@gmail.com>
2024-09-24 18:28:18 +0200

    Remove database info addon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react';
 import { AuthProvider } from 'src/auth/context/jwt';
 import { MotionLazy } from 'src/components/animate/motion-lazy';
 import ProgressBar from 'src/components/progress-bar';
-import { DatabaseInfo, SettingsDrawer, SettingsProvider } from 'src/components/settings';
+import { SettingsDrawer, SettingsProvider } from 'src/components/settings';
 import SnackbarProvider from 'src/components/snackbar/snackbar-provider';
 import ThemeProvider from 'src/theme';
 import { primaryFont } from 'src/theme/typography';
@@ -56,7 +56,6 @@ export default function RootLayout({ children }: Props) {
                     <SettingsDrawer />
                     <ProgressBar />
                     {children}
-                    {process.env.NODE_ENV !== 'production' && <DatabaseInfo />}
                   </SnackbarProvider>
                 </MotionLazy>
               </ThemeProvider>
--- src/components/settings/index.ts
+++ src/components/settings/index.ts
@@ -1,5 +1,4 @@
 export * from './context';
 export * from './types';
 
-export { default as DatabaseInfo } from './database-info';
 export { default as SettingsDrawer } from './drawer';
