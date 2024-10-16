5dc591de676cc53fae66277d91f2fed355233c1a
javi95z <javi.95.zgz@gmail.com>
2024-10-16 18:20:58 +0700

    Check for duplicate external sender addresses when creating a host

--- src/app/api/host/create/route.ts
+++ src/app/api/host/create/route.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import { auth } from 'src/auth/lib/mongodb/auth-mongodb';
 import clientPromise from 'src/auth/lib/mongodb/db-mongo';
 import { getUser } from 'src/auth/lib/mongodb/get-user';
@@ -6,7 +7,6 @@ import { generateHostCrypt, generateLookerStudioUrl } from 'src/sections/host/ut
 export async function POST(request: Request) {
   try {
     const data = await request.json();
-
     const {
       host,
       externalSenderAddresses,
@@ -20,16 +20,34 @@ export async function POST(request: Request) {
     const client = await clientPromise;
     const db = client.db();
     const session = await auth();
-
     const user = await getUser();
 
     // Check if a host with the same name already exists
     const existingHost = await db.collection('hosts').findOne({ host });
     if (existingHost) throw new Error('Profile already created, use different name');
 
+    // Check for duplicate external sender addresses across user hosts
+    const userHosts = user.hosts as ObjectId[];
+    const externalSenderAddressesArray: string[] = externalSenderAddresses.split('\n');
+    const existingEmailHosts = await Promise.all(
+      userHosts.map(async (hostId: ObjectId) => {
+        const hostDoc = await db.collection('hosts').findOne({ _id: hostId });
+        const hostDocExternalSenderAddresses = hostDoc?.userSettings?.externalSenderAddresses;
+        const hasDuplicateEmail = externalSenderAddressesArray.some((email) => {
+          return hostDocExternalSenderAddresses.includes(email.toLowerCase());
+        });
+
+        return hasDuplicateEmail ? hostDoc : null;
+      })
+    );
+
+    const duplicateEmail = existingEmailHosts.find((h) => h !== null);
+    if (duplicateEmail) {
+      throw new Error(`Sender address is already used in host ${duplicateEmail.host}`);
+    }
+
     const hostCrypt = generateHostCrypt(host);
     const lookerStudioUrl = generateLookerStudioUrl([hostCrypt]);
-    const externalSenderAddressesArray = externalSenderAddresses.split('\n');
     const notificationAddressesArray = notificationAddresses.split('\n');
 
     // Create a new host and get the _id of the new document
