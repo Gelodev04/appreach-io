0d744fb8249eeddb93929af6320005ad0bee7a36
javi95z <javi.95.zgz@gmail.com>
2024-10-18 15:53:34 +0700

    Replace graphic on homepage to match mockup

--- src/layouts/common/nav-bottom.tsx
+++ src/layouts/common/nav-bottom.tsx
@@ -1,11 +1,8 @@
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Image from 'next/image';
 
-export default function NavUpgrade() {
-  const url = `https://outreachmagic.io/email-reporting-tutorial/`;
-
+export default function NavBottom() {
   return (
     <Stack
       sx={{
@@ -17,17 +14,13 @@ export default function NavUpgrade() {
       <Stack alignItems="center">
         <Box sx={{ position: 'relative' }}>
           <Image
-            src="/assets/illustrations/dashboard/person.png"
-            width={240}
-            height={240}
+            src="/assets/illustrations/dashboard/menu-graphic.png"
+            width={200}
+            height={200}
             alt="Envelope"
             priority
           />
         </Box>
-
-        <Button variant="contained" href={url} target="_blank" rel="noopener">
-          Access training
-        </Button>
       </Stack>
     </Stack>
   );
