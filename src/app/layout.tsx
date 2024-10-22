f78610566650627dde9699754a1798fd589bb583
Jader Moura <jader.moura.gg@gmail.com>
2024-10-22 10:50:49 -0400

    Added user details to chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import { SettingsDrawer, SettingsProvider } from 'src/components/settings';
 import SnackbarProvider from 'src/components/snackbar/snackbar-provider';
 import ThemeProvider from 'src/theme';
 import { primaryFont } from 'src/theme/typography';
-import Script from 'next/script';
 
 export const viewport = {
   themeColor: '#000000',
@@ -39,32 +38,6 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en" className={primaryFont.className}>
       <body>
-        <Script strategy="afterInteractive">
-          {`
-          window.salesmateSettings = {
-            workspace_id: "7ae0e152-56ea-4ad1-8a71-2a01a2142c2e",
-            app_key: "e5fb65d0-8756-11ef-9245-856ae81ff140",
-            tenant_id: "inboxdaddy.salesmate.io"
-          };
-        `}
-        </Script>
-        <Script strategy="afterInteractive" id="salesmate-widget-loader">
-          {`
-          !function(e, t, a, i, d, n, o) {
-            e.Widget = i;
-            e[i] = e[i] || function() {
-              (e[i].q = e[i].q || []).push(arguments)
-            },
-            n = t.createElement(a), o = t.getElementsByTagName(a)[0],
-            n.id = i, n.src = d,
-            window._salesmate_widget_script_url = d,
-            n.async = 1,
-            o.parentNode.insertBefore(n, o)
-          }(window, document, "script", "loadwidget", "https://inboxdaddy.salesmate.io/messenger-platform/messenger-platform-main.js");
-          loadwidget("init", {});
-          loadwidget("load_widget", "Widget Loading...!");
-        `}
-        </Script>
         <SessionProvider>
           <AuthProvider>
             <SettingsProvider
