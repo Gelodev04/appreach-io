ff0bf105aa9afa67c44110e87f02e73e3c84a5a4
Jader Moura <jader.moura.gg@gmail.com>
2024-10-23 14:11:40 -0400

    add phone attribute to salesmate login

--- src/auth/guard/guest-guard.tsx
+++ src/auth/guard/guest-guard.tsx
@@ -65,6 +65,7 @@ export default function GuestGuard({ children }: Props) {
                   email: "${user?.email}",
                   first_name: "${user?.firstName}",
                   last_name: "${user?.lastName}",
+                  phone: "${user?.phone}",
                 });
                }
             `}
