cc415ba8507d31061520e35f69d6f629a0ee91b2
Jader Moura <jader.moura.gg@gmail.com>
2024-10-26 18:13:44 -0400

    Changed looker url

--- src/sections/host/utils/generate-looker-studio-url.ts
+++ src/sections/host/utils/generate-looker-studio-url.ts
@@ -1,6 +1,6 @@
 export function generateLookerStudioUrl(hostCrypts: string[]): string {
-  const baseUrl = process.env.NEXT_PUBLIC_LOOKER_URL;
-  if (!baseUrl) throw new Error('NEXT_PUBLIC_LOOKER_URL is not defined');
+  const baseUrl = process.env.NEXT_PUBLIC_LIVE_LOOKER_URL;
+  if (!baseUrl) throw new Error('NEXT_PUBLIC_LIVE_LOOKER_URL is not defined');
 
   const idsString = hostCrypts.join(',');
   return baseUrl.replace('{}', idsString);
