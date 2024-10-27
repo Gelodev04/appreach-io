d237ba2c7e455ac087f5d2c61d04712c4144272d
javi95z <javi.95.zgz@gmail.com>
2024-10-27 21:51:50 +0700

    Update active subscription into MongoDB when fetching

--- src/app/api/stripe/subscriptions/route.ts
+++ src/app/api/stripe/subscriptions/route.ts
@@ -1,5 +1,8 @@
 import { auth } from 'auth';
+import { fromUnixTime } from 'date-fns';
 import { NextResponse } from 'next/server';
+import clientPromise from 'src/auth/lib/mongodb/db-mongo';
+import { StripeSubscription } from 'src/types/stripe';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
@@ -12,6 +15,8 @@ export async function GET() {
     const email = session?.user.email;
     if (!email) return NextResponse.json({ error: 'Email is required' }, { status: 400 });
 
+    const client = await clientPromise;
+    const db = client.db();
     const customer = await stripe.customers.list({ email, limit: 1 });
     if (customer.data.length === 0) {
       return NextResponse.json({ error: 'Customer not found' }, { status: 404 });
@@ -30,11 +35,23 @@ export async function GET() {
     // Find the active subscription
     const activeSubscription = subscriptions.data.find(({ status }) =>
       ['active', 'trialing'].includes(status)
-    );
+    ) as StripeSubscription | undefined;
 
     if (!activeSubscription) {
       return NextResponse.json({ error: 'No active subscription found' }, { status: 404 });
     }
+
+    // Update active subscription into MongoDB
+    const stripeObject = {
+      id: activeSubscription.plan.id,
+      status: activeSubscription.status,
+      amount: activeSubscription.plan.amount,
+      amount_decimal: activeSubscription.plan.amount_decimal,
+      start_date: fromUnixTime(activeSubscription.start_date).toISOString(),
+    };
+    await db
+      .collection('userSettings')
+      .updateOne({ 'appLogin.username': session?.user.email }, { $set: { stripe: stripeObject } });
 
     return NextResponse.json(activeSubscription);
   } catch (error) {
