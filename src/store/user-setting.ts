abe89e5cc1cb0bc91a6d6345439f29fd91385888
Dennis Deveza <ddeveza@ymail.com>
2024-11-29 17:09:16 +0800

    fixed hydration error

--- src/store/user-setting.ts
+++ src/store/user-setting.ts
@@ -1,27 +1,38 @@
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
+
 import { UserSettingsPlan } from '@prisma/client';
 
-interface IUserSettings extends UserSettingsPlan {
+interface IUserSettings {
+  plan: UserSettingsPlan | null;
   setUserPlan: (plan: UserSettingsPlan) => void; // Updated to accept a parameter
+  setHydrated(): void;
+  hydrated: boolean;
 }
 
 export const useUsersPlanStore = create<IUserSettings>()(
   devtools(
     persist(
-      (set) => ({
-        amount: 0,
-        amount_decimal: '2',
-        checkout_session_id: '2',
-        current_period_end: new Date(),
-        lookup_key: '1',
-        price_id: '2',
-        start_date: new Date(),
-        status: 'test',
-        subscription_id: 'test',
-        setUserPlan: (plan) => set(() => ({ ...plan })),
-      }),
-      { name: 'usersPlanStore' }
+      immer((set) => ({
+        hydrated: false,
+        plan: null,
+        setUserPlan: (plan: UserSettingsPlan) =>
+          set((state) => {
+            state.plan = plan;
+          }),
+        setHydrated() {
+          set({ hydrated: true });
+        },
+      })),
+      {
+        name: 'usersPlanStore',
+        onRehydrateStorage() {
+          return (state, error) => {
+            if (!error) state?.setHydrated();
+          };
+        },
+      }
     ),
     { enabled: process.env.NODE_ENV === 'development' }
   )
