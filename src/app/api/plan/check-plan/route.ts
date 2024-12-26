06c965f7a626d24c1a30f4d2fdeaf730c3e3aa38
Dennis Deveza <ddeveza@ymail.com>
2024-12-26 15:21:12 +0800

    test if token works

--- src/app/api/plan/check-plan/route.ts
+++ src/app/api/plan/check-plan/route.ts
@@ -1,15 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { auth } from 'src/auth/lib/mongodb/auth-mongodb';
 import { getUserSettingsById } from 'src/services/db/user-settings';
 
 // src/app/api/plan/check-plan/route.ts
 export async function GET(req: NextRequest) {
   try {
     // Example: Extract query parameters from the request
-    const session = await auth();
-    const id = session?.user.id;
-    if (!id)
-      return NextResponse.json({ error: 'Id is not found or unauthenticated.' }, { status: 500 });
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id'); // Replace 'param' with your actual query parameter name
+
+    if (!id) return NextResponse.json({ error: 'Id is not found.' }, { status: 500 });
     // Example: Process the request (this could be a database call, etc.)
     const { plan } = await getUserSettingsById(id, { plan: true });
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -13,11 +13,20 @@ const isTrialExpiredConfigRoute = ['dashboard', 'senders', 'profiles', 'seeds'];
 export default NextAuth(authConfig).auth;
 
 export async function middleware(req: NextRequest) {
+  console.log('Request Headers:', req.headers); // Log request headers
+  const token = await getToken({ req, secret: env.NEXTAUTH_SECRET! });
+  console.log('Token:', token);
+  const userId = token?.sub; // Extract user ID from the token (assuming 'sub' contains the user ID)
   const currentPath = req.nextUrl.pathname;
   const isKeywordIncluded = isTrialExpiredConfigRoute.some((route) => currentPath.includes(route));
-
-  if (isKeywordIncluded) {
-    const { data } = await axios.get<UserSettingsPlan>(`${req.nextUrl.origin}/api/plan/check-plan`);
+  console.log({ userId, currentPath, isKeywordIncluded, origin: req.nextUrl.origin });
+  if (userId && isKeywordIncluded) {
+    const { data } = await axios.get<UserSettingsPlan>(
+      `${req.nextUrl.origin}/api/plan/check-plan`,
+      {
+        params: { id: userId },
+      }
+    );
 
     if (!data?.current_period_end) return NextResponse.next();
 
