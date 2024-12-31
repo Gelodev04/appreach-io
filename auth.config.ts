938bf446071cfdc2d6c5789fbc166d74813b7021
Dennis Deveza <ddeveza@ymail.com>
2024-12-31 16:46:24 +0800

    reverted back

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,20 @@
-import { NextAuthConfig } from 'next-auth';
+import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
-  session: {
-    strategy: 'jwt',
+  pages: {
+    signIn: '/auth/login',
   },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      }
 
+      return true;
+    },
+  },
   providers: [],
 } satisfies NextAuthConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,9 @@
-import { UserSettingsPlan } from '@prisma/client';
 import { authConfig } from 'auth.config';
-import axios from 'axios';
 import NextAuth from 'next-auth';
-import { NextRequest } from 'next/server';
 
-const { auth } = NextAuth(authConfig);
-
-export async function middleware(request: NextRequest) {
-  const { nextUrl } = request;
-  const session = await auth();
-  if (session) {
-    const { user } = session;
-    const url = `${nextUrl.origin}/api/plan/check-trial`;
-    console.log({ headers: request.headers });
-    const { data } = await axios.get<UserSettingsPlan>(url, {
-      params: { email: user.email },
-    });
-
-    console.log({ session, data });
-  }
-}
+export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
