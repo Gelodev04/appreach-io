3530c193d8f11c0aab5f204f66a9be72ac151bae
Dennis Deveza <ddeveza@ymail.com>
2024-12-31 10:36:49 +0800

    test

--- auth.config.ts
+++ auth.config.ts
@@ -1,18 +1,14 @@
-import { UserSettingsPlan } from '@prisma/client';
-import axios from 'axios';
 import type { NextAuthConfig } from 'next-auth';
 import { getToken } from 'next-auth/jwt';
 import { env } from 'src/data/env/server';
-import { getUserSettingsByEmail } from 'src/services/db/user-settings';
 
 const isTrialExpiredConfigRoute = ['dashboard', 'senders', 'profiles', 'seeds'];
 export const authConfig = {
-  pages: {
-    signIn: '/auth/login',
-  },
   callbacks: {
     async authorized({ auth, request }) {
       console.log({ id: auth?.user.id });
+      const token = await getToken({ req: request, secret: env.NEXTAUTH_SECRET as string });
+      console.log({ token });
       const isLoggedIn = !!auth?.user;
 
       const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard');
