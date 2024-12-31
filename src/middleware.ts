cdb1dd4c34021110d4d7df6de8b86eea9cea9d2f
Dennis Deveza <ddeveza@ymail.com>
2024-12-31 11:40:26 +0800

    added token

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,22 +9,22 @@ import { paths } from './routes/paths';
 
 const isTrialExpiredConfigRoute = ['dashboard', 'senders', 'profiles', 'seeds'];
 export async function middleware(req: NextRequest) {
-  console.log('Request Headers:', req.headers); // Log request headers
   const token = await getToken({
     req,
     secret: env.NEXTAUTH_SECRET as string,
-    secureCookie: true,
+    secureCookie: process.env.NODE_ENV === 'production',
   });
   console.log('Token:', token);
   const userId = token?.sub; // Extract user ID from the token (assuming 'sub' contains the user ID)
   const currentPath = req.nextUrl.pathname;
   const isKeywordIncluded = isTrialExpiredConfigRoute.some((route) => currentPath.includes(route));
-  console.log({ userId, currentPath, isKeywordIncluded, origin: req.nextUrl.origin });
+
   if (userId && isKeywordIncluded) {
     const { data } = await axios.get<UserSettingsPlan>(
       `${req.nextUrl.origin}/api/plan/check-plan`,
       {
         params: { id: userId },
+        headers: { Authorization: `Bearer ${token}` },
       }
     );
 
