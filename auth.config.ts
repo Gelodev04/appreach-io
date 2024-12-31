efb2001865ad49542dc2abe3d252b92880293f95
Dennis Deveza <ddeveza@ymail.com>
2024-12-31 15:08:49 +0800

    fixed middleware in next-auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,18 +1,39 @@
+import { UserSettingsPlan } from '@prisma/client';
+import axios from 'axios';
 import type { NextAuthConfig } from 'next-auth';
+import { STRIPE } from 'src/config-global';
+import { env } from 'src/data/env/server';
+import { paths } from 'src/routes/paths';
 
+const isTrialExpiredConfigRoute = ['dashboard', 'senders', 'profiles', 'seeds'];
 export const authConfig = {
   pages: {
     signIn: '/auth/login',
   },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      }
 
+  callbacks: {
+    async authorized({ auth, request }) {
+      const isLoggedIn = !!auth?.user;
+
+      const currentPath = request.nextUrl.pathname;
+      const isKeywordIncluded = isTrialExpiredConfigRoute.some((route) =>
+        currentPath.includes(route)
+      );
+
+      if (isLoggedIn && isKeywordIncluded) {
+        const url = `${request.nextUrl.origin}/api/plan/check-trial`;
+        const { data } = await axios.get<UserSettingsPlan>(url, {
+          params: { email: auth.user.email },
+        });
+
+        if (!data?.current_period_end || data.lookup_key !== STRIPE.subscriptions.trial.key)
+          return true;
+        const trial_end_date = new Date(data.current_period_end);
+
+        return trial_end_date < new Date()
+          ? Response.redirect(new URL(paths.checkout.root, request.nextUrl))
+          : true;
+      }
       return true;
     },
   },
---
+++ src/app/api/plan/check-trial/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getUserSettingsByEmail } from 'src/services/db/user-settings';
+
+// src/app/api/plan/check-plan/route.ts
+export async function GET(req: NextRequest) {
+  try {
+    // Example: Extract query parameters from the request
+    const { searchParams } = new URL(req.url);
+    const email = searchParams.get('email'); // Replace 'param' with your actual query parameter name
+
+    if (!email) return NextResponse.json({ error: 'email is not found.' }, { status: 500 });
+    // Example: Process the request (this could be a database call, etc.)
+    const userSettings = await getUserSettingsByEmail(email, { plan: true });
+
+    // Return a successful response
+    return NextResponse.json(userSettings?.plan, { status: 200 });
+  } catch (error) {
+    // Handle any errors that occur
+    return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
+  }
+}
