e8baeeef4a1970fe358e8c2fc9173435d3e85f16
Aj Deveza <ajdvz0423@gmail.com>
2025-01-06 08:46:33 +0800

    refactor: update userSettings model to use planPermissionsAssigned and planPermissionsUsed; modify user settings retrieval in HostsEditPage

--- src/app/(pages)/profiles/edit/[hostId]/page.tsx
+++ src/app/(pages)/profiles/edit/[hostId]/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default async function HostsEditPage({ params }: { params: { hostId: string } }) {
   const { hostId } = params;
   const host = await getHostById(hostId);
-  const data = await getUserSettings();
+  const data = await getUserSettings({ planPermissionsAssigned: true });
   console.log({ data });
 
   return <HostContainer currentItem={host} />;
