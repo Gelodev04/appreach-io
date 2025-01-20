33b78b2d09bdad359d06b4cc1c51401ad9119459
Aj Deveza <ajdvz0423@gmail.com>
2025-01-20 17:53:15 +0800

    Refactor Email Validator page; update import paths for header and table components

--- src/app/(pages)/email-validator/page.tsx
+++ src/app/(pages)/email-validator/page.tsx
@@ -2,7 +2,8 @@ import { Container } from '@mui/material';
 import { ItemUsageDisplay } from 'src/components/item-usage-tracker/item-usage-display';
 import { getAllEmailValidator } from 'src/services/db/email-validator';
 import { getEmailValidatorPlanPermissions } from 'src/services/db/user-settings';
-import { EmailValidatorHeader, EmailValidatorTable } from './_components';
+import { EmailValidatorHeader } from './_components/email-validator-header';
+import { EmailValidatorTable } from './_components/email-validator-table';
 
 export const metadata = {
   title: 'Email Validator | Inbox Daddy',
