cb78de88e07a31a02efe6796a9713edfeca0cfe1
Aj Deveza <ajdvz0423@gmail.com>
2025-02-07 19:04:11 +0800

    Refactor file upload handling in upload-file-to-signed-url to improve error handling and streamline the upload process

--- src/utils/upload-file-to-signed-url.ts
+++ src/utils/upload-file-to-signed-url.ts
@@ -24,22 +24,21 @@ export const handleFileUpload = async (form: FormData) => {
     const signedUrl = await getSignedUrl(file.name);
 
     // Step 2: Upload file directly to Cloud Storage
-    if (signedUrl.url) {
-      const response = await uploadFileToSignedUrl(file, signedUrl.url);
-
-      // Step 3: Finalize the upload only if the upload was successful
-      if (response) {
-        const permanentUrlData = await finalizeUpload(file.name);
-
-        return { url: permanentUrlData.url };
-      } else {
-        console.error('File upload failed. Skipping finalizeUpload.');
-        return { error: 'File upload failed.' };
-      }
-    } else {
+    if (!signedUrl.url) {
       console.error('Signed URL is not available');
       return { error: 'Signed URL not available.' };
     }
+
+    const response = await uploadFileToSignedUrl(file, signedUrl.url);
+
+    // Step 3: Finalize the upload only if the upload was successful
+    if (!response) {
+      console.error('File upload failed. Skipping finalizeUpload.');
+      return { error: 'File upload failed.' };
+    }
+
+    const permanentUrlData = await finalizeUpload(file.name);
+    return { url: permanentUrlData.url };
   } catch (error) {
     console.error('Error on uploading file:', error);
     return { error: 'Error on getting signed url.' };
