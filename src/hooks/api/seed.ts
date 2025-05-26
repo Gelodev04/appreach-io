13b614aa9ef2519442af01f33ad55b11174258be
Aj Deveza <ajdvz0423@gmail.com>
2025-05-26 13:17:08 +0800

    refactor: remove assignedCount from seed settings response and hooks

--- src/hooks/api/seed.ts
+++ src/hooks/api/seed.ts
@@ -27,15 +27,13 @@ export function useGetSeeds() {
 export function useGetSeedSettings() {
   const URL = endpoints.seed.settings;
   const { data, error, isValidating } = useSWR(URL, fetcher);
-
   const memoizedValue = useMemo(
     () => ({
       hosts: (data?.hosts as IHost[]) || [],
-      assignedCount: (data?.assignedCount as number) || 0,
       error,
       validating: isValidating,
     }),
-    [data?.assignedCount, data?.hosts, error, isValidating]
+    [data?.hosts, error, isValidating]
   );
 
   return memoizedValue;
