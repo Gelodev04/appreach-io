38298df5729b32268fc28c6ea85fcae7888866a5
Aj Deveza <ajdvz0423@gmail.com>
2025-07-23 15:46:20 +0800

    fix: access denied in sharable page, removed the api end point for getting url

--- src/app/sharable/[access_token]/_components/sharable-looker-studio.tsx
+++ src/app/sharable/[access_token]/_components/sharable-looker-studio.tsx
@@ -1,10 +1,7 @@
 'use client';
 
-import { Alert } from '@mui/material';
 import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
-import Error from 'src/components/error/error';
-import { useGetLookerStudioUrl } from 'src/hooks/api/looker-studio';
 import { useResponsive } from 'src/hooks/use-responsive';
 import { generateLookerStudioUrl } from 'src/sections/host/utils';
 import { LookerStudioSkeleton } from 'src/sections/looker-studio/looker-studio-skeleton';
@@ -16,7 +13,6 @@ export const SharableLookerStudio = ({
   accessToken: string;
   defaultLookerStudioUrl?: string;
 }) => {
-  const { url, urlLoading, urlError, warningMessage } = useGetLookerStudioUrl();
   const [lookerStudioUrl, setLookerStudioUrl] = useState<string>('');
   const lgUp = useResponsive('up', 'lg');
 
@@ -27,22 +23,6 @@ export const SharableLookerStudio = ({
     }
   }, [accessToken, defaultLookerStudioUrl]);
 
-  const renderSkeleton = <LookerStudioSkeleton />;
-
-  const renderError = (
-    <Error
-      filled
-      title={`${urlError?.status}`}
-      description={`${urlError?.message}`}
-      sx={{ py: 10 }}
-    />
-  );
-
-  const renderWarning = (
-    <Alert variant="standard" severity="warning" sx={{ mt: 1, mb: 2 }}>
-      {warningMessage}
-    </Alert>
-  );
   const renderLookerStudioIframe = (
     <iframe
       src={lookerStudioUrl}
@@ -52,12 +32,5 @@ export const SharableLookerStudio = ({
     />
   );
 
-  return (
-    <Box sx={{ height: '100%' }}>
-      {warningMessage && renderWarning}
-      {urlLoading && renderSkeleton}
-      {urlError && renderError}
-      {lookerStudioUrl && renderLookerStudioIframe}
-    </Box>
-  );
+  return <Box sx={{ height: '100%' }}>{lookerStudioUrl && renderLookerStudioIframe}</Box>;
 };
