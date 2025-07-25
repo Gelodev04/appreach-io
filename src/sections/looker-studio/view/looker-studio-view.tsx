404fdeb17dbbf3e70765769fc0fdc9b224795a1c
Aj Deveza <ajdvz0423@gmail.com>
2025-07-25 21:02:08 +0800

    refactor: update Looker Studio URL generation logic

--- src/sections/looker-studio/view/looker-studio-view.tsx
+++ src/sections/looker-studio/view/looker-studio-view.tsx
@@ -7,14 +7,14 @@ import Error from 'src/components/error/error';
 import { useGetHosts } from 'src/hooks/api/host';
 import { useGetLookerStudioUrl } from 'src/hooks/api/looker-studio';
 import { useResponsive } from 'src/hooks/use-responsive';
-import { generateLookerStudioOld } from 'src/sections/host/utils/generate-looker-studio-old';
+import { generateLookerStudioUrl } from 'src/sections/host/utils';
 import { LookerStudioSkeleton } from '../looker-studio-skeleton';
 
 export default function LookerStudioView() {
   const { url, urlLoading, urlError, warningMessage } = useGetLookerStudioUrl();
   const { hosts } = useGetHosts();
   const [mappedTokens, setMappedtokens] = useState<string[]>([]);
-
+  const [lookerStudioUrl, setLookerStudioUrl] = useState<string>('');
   const lgUp = useResponsive('up', 'lg');
 
   // Use useEffect to map hosts and set token access into mappedTokens
@@ -24,6 +24,13 @@ export default function LookerStudioView() {
       setMappedtokens(mapped);
     }
   }, [hosts]);
+
+  // Generate Looker Studio URL asynchronously
+  useEffect(() => {
+    if (mappedTokens.length > 0) {
+      generateLookerStudioUrl(mappedTokens, url).then(setLookerStudioUrl);
+    }
+  }, [mappedTokens, url]);
 
   const renderSkeleton = <LookerStudioSkeleton />;
 
@@ -44,7 +51,7 @@ export default function LookerStudioView() {
 
   const renderLookerStudioIframe = (
     <iframe
-      src={generateLookerStudioOld(mappedTokens, url)}
+      src={lookerStudioUrl}
       width="100%"
       title="Looker Studio Dashboard"
       style={{ borderRadius: '10px', border: 'none', height: lgUp ? '100%' : '100vh' }}
@@ -56,7 +63,7 @@ export default function LookerStudioView() {
       {warningMessage && renderWarning}
       {urlLoading && renderSkeleton}
       {urlError && renderError}
-      {url && renderLookerStudioIframe}
+      {lookerStudioUrl && renderLookerStudioIframe}
     </Box>
   );
 }
